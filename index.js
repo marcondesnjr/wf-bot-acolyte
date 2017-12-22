a8e4dc1e89073e6de2d1c3b1fef0896db6ff39b4
José Marcondes N Junior <junior3917@gmail.com>
2017-12-22 12:16:55 -0300

    Versão 0.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const TOKEN = process.env.TOKEN;
+const TOKEN = "MzkzNjIzMTA3NTA5ODEzMjQ4.DR4sLg.MppfpRUzQlP03F0OuwD1NdH3OUY";
 const WorldState = require('warframe-worldstate-parser');
 const worldstateData = require('warframe-worldstate-data');
 
@@ -11,6 +11,29 @@ var bot = new Discord.Client();
 
 bot.on("ready", function(){
     console.log("ready");
+
+    setInterval(function(){
+        console.log("Entrou");
+    var request = require('request');
+request.get('http://content.warframe.com/dynamic/worldState.php', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var csv = body;
+        console.log("Pegou estado");
+        var ws = new WorldState(csv);
+        console.log(ws.persistentEnemies[0].isDiscovered);
+        var status = ws.persistentEnemies[0].isDiscovered;
+            if(status != statusAnterior){
+                console.log("Diferente");            
+                var channel = bot.channels.get('393626375829061642');
+                channel.sendMessage("<@&393660703484936192>\n"+ws.persistentEnemies[0].toString());
+                statusAnterior = status;
+            }else{
+
+            }
+        
+    }
+});
+}, 30000);
     
 });
 
@@ -21,23 +44,5 @@ bot.on("message", function(message){
 bot.login(TOKEN);
 
 
-setInterval(function(){
-    var request = require('request');
-request.get('http://content.warframe.com/dynamic/worldState.php', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var csv = body;
-        var ws = new WorldState(csv);
-        if(ws.persistentEnemies[0].isDiscovered){
-            var status = true;
-            if(status != statusAnterior){
-                console.log("Foi");
-                bot.channels.get('393626375829061642').sendMessage("<@&393660703484936192>\n"+ws.persistentEnemies[0].toString());
-                statusAnterior = status;
-            }else{
 
-            }
-        }
-    }
-});
-}, 30000);
 

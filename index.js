2ed6cbe298b6577a0da3188be8e7a2fd7cc56997
José Marcondes N Junior <junior3917@gmail.com>
2017-12-22 18:09:10 -0300

    Novo candidato 1.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TOKEN = "MzkzNjIzMTA3NTA5ODEzMjQ4.DR4sLg.MppfpRUzQlP03F0OuwD1NdH3OUY";
 const WorldState = require('warframe-worldstate-parser');
 const worldstateData = require('warframe-worldstate-data');
 
-var statusAnterior = false;
+var statusAnterior = true;
 
 var enemy = worldstateData.persistentEnemy;
 
@@ -23,42 +23,9 @@ request.get('http://content.warframe.com/dynamic/worldState.php', function (erro
         console.log(ws.persistentEnemies[0].isDiscovered);
         var status = ws.persistentEnemies[0].isDiscovered;
             if(status != statusAnterior){
-                console.log("Diferente");            
-                var channel = bot.channels.get('393626375829061642');
-
-
-                var embedp = {
-                  "content": "<@&393660703484936192>",
-                  "embed": {
-                    "title": "O acolyte ".concat(status?"apareceu!":"desapareceu!"),
-                    "description": "Ultima vez visto em: "+ ws.persistentEnemies[0].lastDiscoveredAt,
-                    "color": 2327226,
-                    "thumbnail": {
-                      "url": "https://vignette.wikia.nocookie.net/warframe/images/e/ec/StrikerAcolyte.png"
-                    },
-                    "author": {
-                      "name": bot.user.username,
-                      "icon_url": bot.user.avatarURL
-                    }
-                  }
-                }
-
-
-                var embed = new Discord.RichEmbed()
-                    .setTitle("O acolyte ".concat(status?"apareceu!":"desapareceu!"))
-                    .setAuthor(bot.user.username, bot.user.avatarURL)
-
-                    .setColor(0x00AE86)
-                    .setThumbnail("https://vignette.wikia.nocookie.net/warframe/images/e/ec/StrikerAcolyte.png")
-                    .setDescription("Ultima vez visto em: "+ ws.persistentEnemies[0].lastDiscoveredAt)
-                    .setTimestamp();
-
-
-
-
-                channel.send("<@&393660703484936192>",embedp);
-                
-                statusAnterior = status;
+                console.log("Diferente");
+                var enemy = ws.persistentEnemies[0]         
+                sendMsg(status, enemy).then(function(){statusAnterior = status},function(){console.log("Erro, tentando novamente")});    
             }else{
 
             }
@@ -73,8 +40,33 @@ bot.on("message", function(message){
     console.log(message.content);
 });
 
-bot.login(TOKEN);
+function main(){
+    bot.login(TOKEN).then(function(){console.log("logou")}, function(){main()});
+}
 
+main();
 
+function sendMsg(status,enemy){
+    var channel = bot.channels.get('393626375829061642');
+    var embedp = {
+      "content": "<@&393660703484936192>",
+      "embed": {
+        "title": "O acolyte ".concat(status?"apareceu!":"desapareceu!"),
+        "description": "Ultima vez visto em: "+ enemy.lastDiscoveredAt + " com " +enemy.healthPercent+ " de vida",
+        "color": 2327226,
+        "timestamp": new Date(),
+        "thumbnail": {
+          "url": "https://vignette.wikia.nocookie.net/warframe/images/e/ec/StrikerAcolyte.png"
+        },
+        "author": {
+          "name": bot.user.username,
+          "icon_url": bot.user.avatarURL
+        }
+      }
+    }
+
+    return channel.send("<@&393660703484936192>",embedp);
+
+}
 
 

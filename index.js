c87c73b87b6937a5fbb197580750f053c6c343e0
José Marcondes N Junior <junior3917@gmail.com>
2017-12-22 09:30:32 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const TOKEN = process.env.TOKEN;
+const WorldState = require('warframe-worldstate-parser');
+
+var statusAnterior = false;
+
+var enemy = worldstateData.persistentEnemy;
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    console.log("ready");
+    
+});
+
+bot.on("message", function(message){
+    console.log(message.content);
+});
+
+bot.login(TOKEN);
+
+
+setInterval(function(){
+    var request = require('request');
+request.get('http://content.warframe.com/dynamic/worldState.php', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var csv = body;
+        var ws = new WorldState(csv);
+        if(ws.persistentEnemies[0].isDiscovered){
+            var status = true;
+            if(status != statusAnterior){
+                console.log("Foi");
+                bot.channels.get('393626375829061642').sendMessage("<@&393660703484936192>\n"+ws.persistentEnemies[0].toString());
+                statusAnterior = status;
+            }else{
+
+            }
+        }
+    }
+});
+}, 30000);
+
+
+setInterval(() => {
+ http.get('http://discordjs-heroku.herokuapp.com');
+}, 900000);
+
